a79707663eba6ab5641dda9012038ce4d5b7a441
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2020-01-29 12:48:26 -0500

    init

---
+++ src/components/Loading.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="loading"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Loading />
+    <PetList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Loading from '@/components/Loading.vue'
+import PetList from '@/components/PetList.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Loading,
+    PetList
   }
 }
 </script>
