63ace7b7d6f8c1318a4ca0add51f456fd840157c
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2020-01-31 12:47:56 -0500

    basic styling

--- src/components/Pet.vue
+++ src/components/Pet.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="pet">
-    <!-- Thumbnail -->
-    <div
-      v-if="pet.photos && pet.photos.length"
-      class="pet__thumbnail"
-    >
-      <img :src='pet.photos[0].small'>
+    <div class="card__top">
+      <!-- Thumbnail -->
+      <div
+        v-if="pet.photos && pet.photos.length"
+        class="pet__thumbnail"
+      >
+        <img :src='pet.photos[0].small'>
+      </div>
+      <div
+        v-else
+        class="pet__thumbnail"
+      >
+        <img src='https://cdn.pixabay.com/photo/2014/04/03/00/42/footprints-309158_960_720.png'>
+      </div>
+      <div class="card__top details">
+        <h3 class="pet__name">{{ pet.name }}</h3>
+        <div class="pet__species">{{ pet.species }}</div>
+        <div class="pet__gender">{{pet.gender}}</div>
+        <span class="pet__breed">Breed: {{ pet.breeds.primary }}</span> <span v-if="pet.breeds.secondary"> + {{ pet.breeds.secondary}}</span>
+        <div class="pet__age">Age: {{ pet.age }}</div>
+        <div class="pet__location">{{pet.contact.address.city}}, {{pet.contact.address.state}}</div>
+        <a :href=pet.url>Learn More</a>
+      </div>
     </div>
     <div class="pet__details">
-      <h3 class="pet__name">{{ pet.name }}</h3>
+      <h1 class="pet__name">{{ pet.name }}</h1>
       <div class="pet__species">{{ pet.species }}</div>
-      <div class="pet__gender">{{pet.gender}}</div>
       <span class="pet__breed">Breed: {{ pet.breeds.primary }}</span> <span v-if="pet.breeds.secondary"> + {{ pet.breeds.secondary}}</span>
-      <div class="pet__age">Age: {{ pet.age }}</div>
-      <div class="pet__location">{{pet.contact.address.city}}, {{pet.contact.address.state}}</div>
-      <a :href=pet.url>Learn More</a>
     </div>
   </div>
 </template>
@@ -28,13 +41,50 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
 .pet {
+  width: 180px;
+  min-height: 180px;
   margin: 4px;
+  border-radius: 4px;
+  text-align: center;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+
+  &:hover .card__top {
+    min-height: 180px;
+
+    &.details {
+      display: block;
+    }
+  }
+  &:hover .pet__details {
+    display: none;
+  }
+}
+.card__top {
   background-color: blanchedalmond;
-  flex: 200px;
-  display: flex;
-  flex-direction: column;
+  width: 100%;
+  overflow: hidden;
+
+  &.details {
+    display: none;
+  }
 }
 .pet__details {
+  min-height: 180px;
+}
+.pet__thumbnail {
+  width: 90px;
+  height: 90px;
+  margin: 0 auto 15px auto;
+  & img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 50%;
+  }
 }
 </style>
