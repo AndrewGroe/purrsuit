588994787e49d69141091f716a14302b5ff56531
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2020-02-15 17:37:09 -0500

    remove boilerplate comments

--- src/router/index.js
+++ src/router/index.js
@@ -14,18 +14,12 @@ const routes = [
     path: '/pets/:category/:page',
     name: 'pets',
     props: true,
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Pets.vue')
+    component: () => import('../views/Pets.vue')
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   }
 ]
 
