c899ac7c3548711439da6dc3a81d8c654334f1d3
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2020-02-20 17:55:35 -0500

    add router nav guards to access store

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store/index.js'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -20,11 +21,45 @@ const routes = [
         // PetCategories will be rendered inside the Pets <router-view>
         // when /pets/categories is matched
         path: 'categories',
-        component: () => import('../components/PetCategories.vue')
+        component: () => import('../components/PetCategories.vue'),
+
+        beforeEnter: (to, from, next) => {
+          store.dispatch('getAllCategories')
+            .then(next())
+        }
       },
       {
         path: 'categories/:category/:page',
-        component: () => import('../components/PetList.vue')
+        component: () => import('../components/PetList.vue'),
+
+        beforeEnter: (to, from, next) => {
+          // Route & Store Match
+          if (
+            store.state.currentCategory === to.params.category &&
+            store.state.currentPage === to.params.page) {
+            console.log('match')
+
+            store.dispatch('getPetsByCategory')
+              .then(next())
+          } else {
+            console.log('no match')
+
+            if (store.state.categories.length < 1) {
+              console.log('blank categories')
+
+              store.dispatch('getAllCategories')
+                .then(() => store.dispatch('setCurrentCategory', to.params.category))
+                .then(() => store.dispatch('setCurrentPage', to.params.page))
+                .then(() => store.dispatch('getPetsByCategory'))
+                .then(next())
+            } else {
+              store.dispatch('setCurrentCategory', to.params.category)
+                .then(() => store.dispatch('setCurrentPage', to.params.page))
+                .then(() => store.dispatch('getPetsByCategory'))
+                .then(next())
+            }
+          }
+        }
       }
     ]
   },
