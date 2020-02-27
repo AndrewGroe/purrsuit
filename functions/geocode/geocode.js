8c9315668d17a9a43d8ee9a030acf37ce00efbdb
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2020-02-27 14:49:58 -0500

    fix gcp geocode request

--- functions/geocode/geocode.js
+++ functions/geocode/geocode.js
@@ -4,11 +4,28 @@ const BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 const GCP_KEY = process.env.GCP_KEY
 
 exports.handler = async (event, context) => {
-  const response = await axios.get(BASE_URL, {
-    params: {
-      latlng: event.queryStringParameters.coords,
-      key: GCP_KEY
+  let response
+  try {
+    response = await axios.get(BASE_URL, {
+      params: {
+        latlng: event.queryStringParameters.coords,
+        key: GCP_KEY
+      }
+    })
+    // handle response
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
     }
-  })
-  return response
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response.data
+    })
+  }
 }
