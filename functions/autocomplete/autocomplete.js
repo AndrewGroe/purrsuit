da30cf1c0ecc93a6b591290d7546be3788331b8a
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2020-02-29 14:54:35 -0500

    add region search suggestions

---
+++ functions/autocomplete/autocomplete.js
@@ -0,0 +1,33 @@
+const Client = require('@googlemaps/google-maps-services-js').Client
+
+const GCP_KEY = process.env.GCP_KEY
+
+exports.handler = async (event, context) => {
+  const client = new Client({})
+  let response
+
+  try {
+    response = await client.placeAutocomplete({
+      params: {
+        input: event.queryStringParameters.input,
+        key: GCP_KEY,
+        types: '(regions)'
+      },
+      timeout: 1000 // milliseconds
+    })
+  } catch (e) {
+    console.log(e)
+    return {
+      statusCode: e.statusCode || 500,
+      body: JSON.stringify({
+        error: e.message
+      })
+    }
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response.data.predictions
+    })
+  }
+}
