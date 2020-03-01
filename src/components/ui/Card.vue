6b2cb001d8cc79161285dc9db8b96347b883e993
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2020-03-01 15:05:29 -0500

    swap height and width min-max

--- src/components/ui/Card.vue
+++ src/components/ui/Card.vue
@@ -52,8 +52,8 @@ img {
   display: block;
   top: 50%;
   left: 50%;
-  min-height: 100%;
-  max-width: 100%;
+  max-height: 100%;
+  min-width: 100%;
   transform: translate(-50%, -50%);
 }
 .overlay {
