ef2bb359aa7bb4da77bc37ee440423dfd5b6e804
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2020-03-09 22:31:32 -0400

    add test for PetList.vue

---
+++ tests/unit/PetList.spec.js
@@ -0,0 +1,48 @@
+import { shallowMount } from '@vue/test-utils'
+import PetList from '@/components/PetList.vue'
+
+const factory = (storeData) => {
+  return shallowMount(PetList, {
+    mocks: {
+      $store: {
+        state: {
+          ...storeData
+        }
+      }
+    },
+    stubs: ['router-link']
+  })
+}
+
+describe('PetList.vue', () => {
+  it('renders a list of pets', () => {
+    const wrapper = factory({ pets: [{}, {}, {}] })
+
+    expect(wrapper.findAll({ name: 'PetListItem' }).length).toBe(3)
+  })
+})
+
+describe('PetList.vue', () => {
+  it('renders a loading spinner', () => {
+    const wrapper = factory({ pets: [], loading: true })
+
+    expect(wrapper.find({ name: 'LoadingSpinner' }).exists()).toBe(true)
+  })
+})
+
+describe('PetList.vue', () => {
+  it('renders a location modal', () => {
+    const wrapper = factory({ pets: [], loading: false })
+    wrapper.setData({ showModal: true })
+
+    expect(wrapper.find({ name: 'LocationModal' }).exists()).toBe(true)
+  })
+})
+
+describe('PetList.vue', () => {
+  it('renders pagination controls', () => {
+    const wrapper = factory({ pets: [{}, {}, {}], loading: false })
+
+    expect(wrapper.find('.pagination').exists()).toBe(true)
+  })
+})
